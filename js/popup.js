8b5767a58ad12712bad01cf0383351486241d8b8
Nutan Ganaraj <nutanmungila@gmail.com>
2015-07-16 17:41:58 +0100

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -3,22 +3,22 @@ import React from 'react';
 
 class Popup extends React.Component{
 
-  	constructor(props) {
-	    super(props);
-	    this.state = {itm:"aa"};
-	    this.onClickhandler = this.onClickhandler.bind(this);
-  }
+  	constructor(props){
+		super(props);
+		this.state = {itm:"aa"};
+		this.onClickhandler = this.onClickhandler.bind(this);
+  	}
   
   	componentWillMount(){
-  	var elemts = this.props.elements; 
-     // console.log(elemts);//returns ["aa", "bb", "cc"]  How to pass these list of elements to popup class ??!!
-  }
+	  	var elemts = this.props.elements; 
+	     	//console.log(elemts);//returns ["aa", "bb", "cc"]  How to pass these list of elements to popup class ??!!
+  	}
 
   	onClickhandler(e){
-  	var val = React.findDOMNode(this.refs.menuitems).value; // Something wrong here need to get selected (clicked) value
-  	this.props.onItemSelected(val);
-	console.log(val);
-  }
+	  	var val = React.findDOMNode(this.refs.menuitems).value; // Something wrong here need to get selected (clicked) value
+	  	this.props.onItemSelected(val);
+		console.log(val);
+  	}
 
 	render(){
 		// need to get values from Dropdown and display those values may be we can use array mapping 
@@ -28,7 +28,7 @@ class Popup extends React.Component{
 					{ this.props.elements.map(function (item) { return (<div ref="menuitems">{item}</div>); } ) }
 				</div>
 			</div>
-			);
+		      );
 	}
 
 }
