3430c1aacc4d4b6fcf812d624cd12c85a5bcd968
nutanmungila <nutanmungila@gmail.com>
2015-07-18 13:21:39 +0100

    Dropdown Component

--- js/dropdown.js
+++ js/dropdown.js
@@ -17,12 +17,20 @@ class Dropdown extends React.Component {
   }
   
   onClickbox(e){
+      e.nativeEvent.stopImmediatePropagation();
       this.setState({showPopup:!(this.state.showPopup)});
+  }
+
+  componentDidMount(){
+    document.addEventListener("click",()=>{ 
+      this.setState({showPopup:false});
+    })
+
   }
 
   render(){
       return( 
-            <div className="box" ref="menubox" onClick={this.onClickbox}>
+            <div className="box" ref="menubox" onClick={this.onClickbox} >
               <div className="ddText">
               {this.state.selectedItemvalue}
               { this.state.showPopup ? <Popup elts={this.props.items} onItemSelected={this.onItemSelectedHandler}/> : null }
